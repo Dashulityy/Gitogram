48927069609e166ade3096499f2caebc129bdcf0
Dashulityy <95776265+Dashulityy@users.noreply.github.com>
2023-08-21 12:42:58 +0500

    new components

---
+++ src/components/slide/slide.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="c-slide">
+        <div class="slide__head">
+            <div class="slide__progress">
+                <progressBar />
+            </div>
+            <div class="slide__user">
+                <users />
+            </div>
+        </div>
+        <div class="slide__body">
+            <slideContent />
+        </div>
+        <div class="slide__footer">
+            <xbutton />
+        </div>
+    </div>
+</template>
+
+<script>
+import { users } from '../users'
+import { button as xbutton } from '../button'
+import { progress as progressBar } from '../progress'
+import { content as slideContent } from '../content'
+
+export default {
+  components: {
+    users,
+    xbutton,
+    progressBar,
+    slideContent
+  }
+}
+</script>
+<style lang="scss" src="./slide.scss" scoped></style>
