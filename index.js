edea378e291042e112e905533ae4eb457f052414
Dashulityy <95776265+Dashulityy@users.noreply.github.com>
2023-08-21 12:49:02 +0500

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-export { default as home } from './home.vue'
-export { default as logo } from './logo.vue'
-export { default as exit } from './exit.vue'
-export { default as star } from './star.vue'
-export { default as triangle } from './triangle.vue'
-export { default as fork } from './fork.vue'
